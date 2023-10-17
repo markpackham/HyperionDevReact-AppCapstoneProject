01f79a1411e4d7a6ee2a71cd36295023cfc8987d
mark <markpackham1@gmail.com>
2023-10-17 16:11:55 +0100

    add dummy state

--- reactStoreCapstoneProject/src/main.jsx
+++ reactStoreCapstoneProject/src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import App from "./App.jsx";
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
