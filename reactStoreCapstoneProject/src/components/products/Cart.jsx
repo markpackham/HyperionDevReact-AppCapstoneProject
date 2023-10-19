a7b2bee50e3577fec583045e75247a240756cb28
mark <markpackham1@gmail.com>
2023-10-19 10:05:44 +0100

    add price increases for shipping

--- reactStoreCapstoneProject/src/components/products/Cart.jsx
+++ reactStoreCapstoneProject/src/components/products/Cart.jsx
@@ -10,10 +10,26 @@ const Cart = () => {
   const productCart = useSelector((state) => state.productCart);
 
   // Show total price in cart
-  const totalPrice = productCart.reduce((acc, item) => acc + item.price, 0);
+  let totalPrice = productCart.reduce((acc, item) => acc + item.price, 0);
 
+  // Wipe out entire cart
   const handleEmptyCartClick = () => {
-    // Pass in id as payload so correct product is deleted
+    dispatch(emptyCart());
+  };
+
+  // Shipping Purchase
+  const handleEconomyClick = () => {
+    totalPrice++;
+    dispatch(emptyCart());
+  };
+
+  const handleBusinessClick = () => {
+    totalPrice += 5;
+    dispatch(emptyCart());
+  };
+
+  const handlePremiumClick = () => {
+    totalPrice += 10;
     dispatch(emptyCart());
   };
 
@@ -41,10 +57,18 @@ const Cart = () => {
       <button onClick={handleEmptyCartClick} className="btn btn-danger">
         Empty Cart
       </button>
-      <h3 className="mt-2">Shipping Options</h3>
-      <button className="btn btn-secondary">Economy £1</button>
-      <button className="btn btn-primary">Business £5</button>
-      <button className="btn btn-warning">Premium £10</button>
+      <h3 className="mt-2">
+        Shipping Options - Click one to make your purchase
+      </h3>
+      <button onClick={handleEconomyClick} className="btn btn-secondary">
+        Economy £1
+      </button>
+      <button onClick={handleBusinessClick} className="btn btn-primary">
+        Business £5
+      </button>
+      <button onClick={handlePremiumClick} className="btn btn-warning">
+        Premium £10
+      </button>
     </>
   );
 };
