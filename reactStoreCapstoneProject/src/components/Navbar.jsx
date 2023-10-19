dccc35e1e3a1b8808609d56fd4827e5dba516916
mark <markpackham1@gmail.com>
2023-10-19 15:59:53 +0100

    hide login and register links if user already logged in

--- reactStoreCapstoneProject/src/components/Navbar.jsx
+++ reactStoreCapstoneProject/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  // Grab user from store
+  const userName = useSelector((state) => state.userName.userName);
+
   return (
     <nav className="navbar navbar-expand-lg mb-3 rounded">
       <div className="container">
@@ -20,16 +24,21 @@ export default function Navbar() {
               Cart
             </Link>
           </li>
-          <li className=" nav-item">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-          <li className=" nav-item">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
+          {/* Hide unless user is logged out */}
+          {userName === "Logged out" && (
+            <>
+              <li className=" nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className=" nav-item">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
