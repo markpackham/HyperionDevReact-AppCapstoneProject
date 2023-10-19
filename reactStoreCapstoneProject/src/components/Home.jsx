44c153649ada706ded497d638b8bbc06c1f2722b
mark <markpackham1@gmail.com>
2023-10-19 10:32:09 +0100

    treat public directory as root path

--- reactStoreCapstoneProject/src/components/Home.jsx
+++ reactStoreCapstoneProject/src/components/Home.jsx
@@ -6,11 +6,7 @@ const Home = () => {
       <h1>Welcome to the React Store Capstone</h1>
       <h2>Home to some of the best kitchen utensils you will ever find!</h2>
       <div className="storeImageContainer">
-        <img
-          id="storeImage"
-          src="../../public/images/store.jpg"
-          alt="store image"
-        />
+        <img id="storeImage" src="/images/store.jpg" alt="store image" />
       </div>
     </div>
   );
